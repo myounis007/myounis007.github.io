2755662e9e107cb17371027b56fcab2d1221f1de
myounis007 <myounis007@users.noreply.github.com>
2026-06-15 10:56:52 +0000

    deploy: ec5329483a8bfe0c85873f575828d12b53d62b08

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -35,8 +35,20 @@ if (!window._flutter) {
 }
 _flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19cab","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
 
+
 _flutter.loader.load({
-  serviceWorkerSettings: {
-    serviceWorkerVersion: "3821755888" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+  onEntrypointLoaded: async function(engineInitializer) {
+    const appRunner = await engineInitializer.initializeEngine();
+    
+    // Fade out and remove the skeleton loader
+    const loader = document.getElementById('skeleton-loader');
+    if (loader) {
+      loader.classList.add('fade-out');
+      setTimeout(() => {
+        loader.remove();
+      }, 500); // Wait for the CSS transition to complete
+    }
+    
+    await appRunner.runApp();
   }
 });
